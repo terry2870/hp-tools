cc08e1ef8270749cd157a831400c1fa4a3cad407
黄平 <ping.huang@yoho.cn>
2018-01-19 10:41:53 +0800

    loop增加index

--- jsptags/src/main/java/com/hp/jsptags/tags/LoopTag.java
+++ jsptags/src/main/java/com/hp/jsptags/tags/LoopTag.java
@@ -6,6 +6,8 @@ package com.hp.jsptags.tags;
 
 import javax.servlet.jsp.JspException;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * 循环标签
  * 
@@ -16,6 +18,8 @@ public class LoopTag extends BaseTagSupport {
 
 	private static final long serialVersionUID = 1L;
 	private int times = 0;
+	private String index = DEFAULT_INDEX_ATTRIBUTE_NAME;
+	private int indexNum;
 
 	// Set方法设值
 	public void setTimes(int times) {
@@ -24,6 +28,10 @@ public class LoopTag extends BaseTagSupport {
 
 	public int doStartTag() throws JspException {
 		super.doStartTag();
+		indexNum = 0;
+		if (StringUtils.isNotEmpty(this.index)) {
+			request.setAttribute(this.index, this.indexNum);
+		}
 		if (this.times > 0) {
 			this.times--;
 			return EVAL_BODY_INCLUDE;
@@ -33,6 +41,10 @@ public class LoopTag extends BaseTagSupport {
 	}
 
 	public int doAfterBody() throws JspException {
+		indexNum++;
+		if (StringUtils.isNotEmpty(this.index)) {
+			request.setAttribute(this.index, this.indexNum);
+		}
 		if (this.times > 0) {
 			this.times--;
 			// 表示双从标签开始输入
@@ -41,5 +53,14 @@ public class LoopTag extends BaseTagSupport {
 		// 表示结束，忽略标签内部的内容
 		return SKIP_BODY;
 	}
+
+	public void setIndex(String index) {
+		this.index = index;
+	}
+
+	public void setIndexNum(int indexNum) {
+		this.indexNum = indexNum;
+	}
+
 
 }
